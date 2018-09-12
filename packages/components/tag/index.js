073e76881856c3234990fb876b54065cacd79e21
wwxfzll <276272330@qq.com>
2018-09-12 10:26:38 +0800

    update rule

--- packages/components/tag/index.js
+++ packages/components/tag/index.js
@@ -2,9 +2,9 @@
  * @author wwx
  * Date: 18/08/11
  */
-import Tag from './src/tag.vue'
+import Tag from './src/main.vue'
 
-Tag.install = function(Vue) {
+Tag.install = function (Vue) {
   Vue.component(Tag.name, Tag)
 }
 
